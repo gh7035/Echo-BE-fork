6ce1f046045ce591451447f1d9cb0d14d4b41996
설규한 <gh7035@daum.net>
2026-01-22 21:41:52 +0900

    api key 네이밍 수정
    api.key 에서 api_key 로 수정

--- src/main/java/org/example/echo/global/security/ApiKeyFilter.java
+++ src/main/java/org/example/echo/global/security/ApiKeyFilter.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 @Component
 public class ApiKeyFilter extends OncePerRequestFilter{
-    @Value("${api.key}")
+    @Value("${api_key}")
     private String apiKey;
 
 
